f67ccf754620bf697ab1789aaa90a5b1cca7ed67
zmewshaw <60802390+zmewshaw@users.noreply.github.com>
2021-08-31 17:42:18 -0400

    added mnist

--- EMNIST.py
+++ EMNIST.py
@@ -1,6 +1,6 @@
+import idx2numpy
 import numpy as np
-
-class neuralNetwork:
-    def main():
-        print("yes")
-neuralNetwork.main()
+imageFile = "C:/Users/zmews/GitHub/machine-learning/mnist/train-images.idx3-ubyte"
+labelFile = "C:/Users/zmews/GitHub/machine-learning/mnist/train-labels.idx1-ubyte"
+imageArr = idx2numpy.convert_from_file(imageFile)
+labelArr = idx2numpy.convert_from_file(labelFile)
