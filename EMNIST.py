52e0b9f2b597070aa75fb8771a0815c6ad943c6e
zmewshaw <60802390+zmewshaw@users.noreply.github.com>
2021-08-31 18:35:45 -0400

    updated

--- EMNIST.py
+++ EMNIST.py
@@ -1,6 +1,15 @@
 import idx2numpy
 import numpy as np
-imageFile = "C:/Users/zmews/GitHub/machine-learning/mnist/train-images.idx3-ubyte"
-labelFile = "C:/Users/zmews/GitHub/machine-learning/mnist/train-labels.idx1-ubyte"
-imageArr = idx2numpy.convert_from_file(imageFile)
-labelArr = idx2numpy.convert_from_file(labelFile)
+import tensorflow as tf
+from tensorflow import keras
+
+testImages = "C:/Users/zmews/GitHub/machine-learning/mnist/t10k-images.idx3-ubyte"
+testLabels = "C:/Users/zmews/GitHub/machine-learning/mnist/t10k-labels.idx1-ubyte"
+trainImages = "C:/Users/zmews/GitHub/machine-learning/mnist/train-images.idx3-ubyte"
+trainLabels = "C:/Users/zmews/GitHub/machine-learning/mnist/train-labels.idx1-ubyte"
+
+testImageArr = idx2numpy.convert_from_file(testImages)
+testLabelArr = idx2numpy.convert_from_file(testLabels)
+trainImageArr = idx2numpy.convert_from_file(trainImages)
+trainLabelArr = idx2numpy.convert_from_file(trainLabels)
+
