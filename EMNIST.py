cb145733c45dee5e3a50b1091984504d01ae3cce
Zach <zmewshaw@gmail.com>
2021-08-31 19:56:30 -0400

    directory change

--- EMNIST.py
+++ EMNIST.py
@@ -3,13 +3,12 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
-testImages = "C:/Users/zmews/GitHub/machine-learning/mnist/t10k-images.idx3-ubyte"
-testLabels = "C:/Users/zmews/GitHub/machine-learning/mnist/t10k-labels.idx1-ubyte"
-trainImages = "C:/Users/zmews/GitHub/machine-learning/mnist/train-images.idx3-ubyte"
-trainLabels = "C:/Users/zmews/GitHub/machine-learning/mnist/train-labels.idx1-ubyte"
+testImages = "C:/Users/Zach/GitHub/machine-learning/mnist/t10k-images.idx3-ubyte"
+testLabels = "C:/Users/Zach/GitHub/machine-learning/mnist/t10k-labels.idx1-ubyte"
+trainImages = "C:/Users/Zach/GitHub/machine-learning/mnist/train-images.idx3-ubyte"
+trainLabels = "C:/Users/Zach/GitHub/machine-learning/mnist/train-labels.idx1-ubyte"
 
 testImageArr = idx2numpy.convert_from_file(testImages)
 testLabelArr = idx2numpy.convert_from_file(testLabels)
 trainImageArr = idx2numpy.convert_from_file(trainImages)
 trainLabelArr = idx2numpy.convert_from_file(trainLabels)
-
