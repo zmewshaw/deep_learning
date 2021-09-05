ded80e67797d45249daeb2f84cfbc005b4c7fa27
zmewshaw <60802390+zmewshaw@users.noreply.github.com>
2021-09-05 03:22:43 -0400

    changed

--- EMNIST.py
+++ EMNIST.py
@@ -1,14 +1,12 @@
 import idx2numpy
 import numpy as np
 
-testImages = "C:/Users/zmews/GitHub/machine-learning/mnist/t10k-images.idx3-ubyte"
-testLabels = "C:/Users/zmews/GitHub/machine-learning/mnist/t10k-labels.idx1-ubyte"
-trainImages = "C:/Users/zmews/GitHub/machine-learning/mnist/train-images.idx3-ubyte"
-trainLabels = "C:/Users/zmews/GitHub/machine-learning/mnist/train-labels.idx1-ubyte"
+trainImages = "C:/Users/zmews/GitHub/deep-learning/mnist/train-images.idx3-ubyte"
+trainLabels = "C:/Users/zmews/GitHub/deep-learning/mnist/train-labels.idx1-ubyte"
+testImages = "C:/Users/zmews/GitHub/deep-learning/mnist/t10k-images.idx3-ubyte"
+testLabels = "C:/Users/zmews/GitHub/deep-learning/mnist/t10k-labels.idx1-ubyte"
 
-testImageArr = idx2numpy.convert_from_file(testImages)
-testLabelArr = idx2numpy.convert_from_file(testLabels)
 trainImageArr = idx2numpy.convert_from_file(trainImages)
 trainLabelArr = idx2numpy.convert_from_file(trainLabels)
-
-print(testImageArr)
+testSetX = idx2numpy.convert_from_file(testImages)
+testLabelArr = idx2numpy.convert_from_file(testLabels)
