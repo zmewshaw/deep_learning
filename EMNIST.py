4150d4b9175fb215947a33336ba4f5194811a4a2
zmewshaw <60802390+zmewshaw@users.noreply.github.com>
2021-09-05 03:31:18 -0400

    updated

--- EMNIST.py
+++ EMNIST.py
@@ -1,12 +1,25 @@
 import idx2numpy
 import numpy as np
+import matplotlib as plt
 
 trainImages = "C:/Users/zmews/GitHub/deep-learning/mnist/train-images.idx3-ubyte"
 trainLabels = "C:/Users/zmews/GitHub/deep-learning/mnist/train-labels.idx1-ubyte"
 testImages = "C:/Users/zmews/GitHub/deep-learning/mnist/t10k-images.idx3-ubyte"
 testLabels = "C:/Users/zmews/GitHub/deep-learning/mnist/t10k-labels.idx1-ubyte"
 
-trainImageArr = idx2numpy.convert_from_file(trainImages)
-trainLabelArr = idx2numpy.convert_from_file(trainLabels)
-testSetX = idx2numpy.convert_from_file(testImages)
-testLabelArr = idx2numpy.convert_from_file(testLabels)
+trainSetXOrig = idx2numpy.convert_from_file(trainImages)
+trainSetY = idx2numpy.convert_from_file(trainLabels)
+testSetXOrig = idx2numpy.convert_from_file(testImages)
+testSetY = idx2numpy.convert_from_file(testLabels)
+
+# steps:
+# find m_train, m_test, num_px (global)
+# flatten the training and test set (global)
+# standardize the data set (global)
+# sigmoid (function)
+# initialize with 0s (function)
+# propagate (function)
+# optimize (function)
+# predict (function)
+# model (function)
+# logisticRegressionModel = model(trainSetX, trainSetY, testSetX, testSetY, numIterations=TBD, learningRate=TBD)
