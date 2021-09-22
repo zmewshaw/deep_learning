7f06b57450e78d70412c72a969ac266dc9c702e2
zmewshaw <60802390+zmewshaw@users.noreply.github.com>
2021-09-22 14:40:23 -0400

    Updated

--- Practice.py
+++ Practice.py
@@ -1,10 +1,5 @@
 import numpy as np
 
-D1 = np.random.rand(3, 3)
-print(D1)
-D1 = (D1 > .5).astype(int)
-print(D1)
-A1 = np.random.randn(3, 3)
-print(A1)
-A1 *= D1
-print(A1)
+x = np.zeros((4, 4))
+print(x)
+print(np.exp(1, x))
