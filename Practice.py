942605963e8ca5d0f4ed315ee99c4de00838c0a2
zmewshaw <60802390+zmewshaw@users.noreply.github.com>
2021-09-28 23:37:23 -0400

    Updated

--- Practice.py
+++ Practice.py
@@ -1,3 +1,21 @@
-x = [0, 3, 4, 5]
+quarter = 25
+dime = 10
+nickel = 5
 
-print(x[0:3])
+amount_paid = 100
+
+def get_user_input():
+    cost = input("Whats the cost: ")
+    return cost
+
+def calculate_correct_amount_of_change(cost):
+    change_left = amount_paid - cost
+    num_quarters = change_left//quarter
+
+
+def main():
+    cost_of_item = get_user_input()
+    calculate_correct_amount_of_change(cost_of_item)
+
+if 1 != 2:
+    print(3)
